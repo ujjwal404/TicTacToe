1f1ffa59db05fc037f44662ed1d48a095b560ea7
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2021-07-03 22:42:20 +0530

    removed bugs

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,11 +3,25 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button nbButton status="primary" *ngIf="!value">{{ value }}</button>
-    <button nbButton hero status="success" *ngIf="value === 'X'">
+    <button nbButton outline-width="0" status="primary" *ngIf="!value">
+      {{ value }}
+    </button>
+    <button
+      nbButton
+      hero
+      outline-width="0px"
+      status="success"
+      *ngIf="value === 'X'"
+    >
       <h1>{{ value }}</h1>
     </button>
-    <button nbButton hero status="info" *ngIf="value === 'O'">
+    <button
+      nbButton
+      hero
+      outline-width="0px"
+      status="info"
+      *ngIf="value === 'O'"
+    >
       <h1>{{ value }}</h1>
     </button>
   `,
@@ -15,13 +29,19 @@ import { Component, Input } from '@angular/core';
     `
       :host {
         height: 200px;
-        border: 1px gray solid;
+        border: 1px solid gray;
+        outline-width: 0px;
       }
     `,
     `
       button {
         height: 200px;
         width: 200px;
+        border: none;
+      }
+    `,
+    `
+      app-square {
         border: none;
       }
     `,
